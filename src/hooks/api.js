9c9440345b185d5fe31c1b62bf0a199a22e3e6c1
Saumya <saumyakant4421@gmail.com>
2025-08-25 20:05:38 +0530

    Backend API changes

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://store-backend-19bh.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL || 'https://store-backend-19bh.onrender.com/api';
 const api = axios.create({
   baseURL: API_URL,
 });
